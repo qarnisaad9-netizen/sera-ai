8a26ef4819a569971928464f136d5070e740ef3c
qarnisaad9-netizen <qarnisaad9@gmail.com>
2025-12-12 01:10:13 +0300

    update m5azn.js

--- scraper/m5azn.js
+++ scraper/m5azn.js
@@ -1,10 +1,7 @@
-// scraper/m5azn.js
-
 export async function updateM5aznProducts() {
   try {
     console.log("M5azn Scraper Started...");
 
-    // لاحقًا نستبدل هذا بقائمة المنتجات الفعلية من API المخازن الإلكترونية
     const products = [
       { id: "p1", name: "Test Product 1", price: 25 },
       { id: "p2", name: "Test Product 2", price: 40 }
