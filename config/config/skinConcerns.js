a4768f1d68b44362053a1a811d31bb7f85a3c008
qarnisaad9-netizen <qarnisaad9@gmail.com>
2025-12-14 20:17:32 +0300

    Add skinConcerns.js with acne details

---
+++ config/config/skinConcerns.js
@@ -0,0 +1,8 @@
+export const SKIN_CONCERNS = {
+  acne: {
+    ar: "حب الشباب",
+    path: "/skin-care/acne",
+    brands: ["La Roche-Posay", "CeraVe", "The Ordinary", "Bioderma"],
+    types: ["cleanser", "serum", "treatment"]
+  }
+};
