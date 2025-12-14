f312ab0c639143c660d711303db806dd543225ef
qarnisaad9-netizen <qarnisaad9@gmail.com>
2025-12-14 22:12:39 +0300

    Add m5azn best selling links map

---
+++ config/m5aznLinks.js
@@ -0,0 +1,55 @@
+export const m5aznLinks = {
+  acne: {
+    label: "حب الشباب",
+    bestSellingUrl:
+      "https://m5azn.sa/beauty/skin-care/acne?sort=best_selling",
+    suggestedProductTypes: [
+      "cleanser",
+      "serum",
+      "treatment"
+    ]
+  },
+
+  pigmentation: {
+    label: "التصبغات",
+    bestSellingUrl:
+      "https://m5azn.sa/beauty/skin-care/pigmentation?sort=best_selling",
+    suggestedProductTypes: [
+      "serum",
+      "treatment",
+      "mask"
+    ]
+  },
+
+  dryness: {
+    label: "الجفاف",
+    bestSellingUrl:
+      "https://m5azn.sa/beauty/skin-care/dryness?sort=best_selling",
+    suggestedProductTypes: [
+      "cleanser",
+      "moisturizer",
+      "mask"
+    ]
+  },
+
+  sensitivity: {
+    label: "البشرة الحساسة",
+    bestSellingUrl:
+      "https://m5azn.sa/beauty/skin-care/sensitive?sort=best_selling",
+    suggestedProductTypes: [
+      "cleanser",
+      "moisturizer"
+    ]
+  },
+
+  aging: {
+    label: "مكافحة التجاعيد",
+    bestSellingUrl:
+      "https://m5azn.sa/beauty/skin-care/anti-aging?sort=best_selling",
+    suggestedProductTypes: [
+      "serum",
+      "treatment",
+      "mask"
+    ]
+  }
+};
