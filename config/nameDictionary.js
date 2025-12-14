0f2ab17236ca1ea245a343011792544ff0886523
qarnisaad9-netizen <qarnisaad9@gmail.com>
2025-12-14 21:44:17 +0300

    Add product name dictionary

---
+++ config/nameDictionary.js
@@ -0,0 +1,57 @@
+export const nameDictionary = {
+  cleanser: {
+    synonyms: [
+      "غسول",
+      "منظف",
+      "cleanser",
+      "face wash",
+      "gel cleanser",
+      "foam cleanser"
+    ]
+  },
+
+  toner: {
+    synonyms: [
+      "تونر",
+      "toner",
+      "ماء ورد",
+      "astringent"
+    ]
+  },
+
+  serum: {
+    synonyms: [
+      "سيروم",
+      "serum",
+      "essence",
+      "ampoule"
+    ]
+  },
+
+  moisturizer: {
+    synonyms: [
+      "مرطب",
+      "كريم",
+      "moisturizer",
+      "cream",
+      "lotion"
+    ]
+  },
+
+  sunscreen: {
+    synonyms: [
+      "واقي شمس",
+      "sunblock",
+      "sunscreen",
+      "spf"
+    ]
+  },
+
+  mask: {
+    synonyms: [
+      "ماسك",
+      "قناع",
+      "mask"
+    ]
+  }
+};
